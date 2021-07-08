2d8b0dbedf4fb5e7b1d6f7e04599945881048a1e
thatsbetter <52314857+Thatsbetter@users.noreply.github.com>
2021-07-08 10:30:28 +0200

    added Test and completed dummy

---
+++ Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
@@ -0,0 +1,106 @@
+package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
+
+import java.util.Map;
+
+public class Geldbetrag
+{
+    private final int _euroAnteil;
+    private final int _centAnteil;
+    private static final int MAXCENTANTEIL = 99; //vorher 100
+    private static Map<String, Geldbetrag> werteMenge;
+
+    public Geldbetrag(int euro, int cent)
+    {
+        assert cent < MAXCENTANTEIL;
+
+        _euroAnteil = euro;
+        _centAnteil = cent;
+    }
+
+    public static boolean istGueltigerString(String s)
+    {
+        return true;
+    }
+
+    public static boolean istGueltigerEuroAnteil(int euro)
+    {
+        return true;
+    }
+
+    public static boolean istGueltigerCentAnteil(int cent)
+    {
+        return true;
+    }
+
+    /*
+    *
+    
+    * checkt ob es groesser oder kleiner ist
+    
+    *@return
+    */
+    public boolean istGroesserGleich(Geldbetrag g)
+    {
+        return true;
+    }
+
+    public static Geldbetrag selectGeldbetrag(int euroAnteil, int centAnteil)
+    {
+        return null;
+    }
+
+    public static Geldbetrag selectGeldbetrag(int euro)
+    {
+        return null;
+    }
+
+    public static Geldbetrag selectGeldbetrag(String geldBetrag)
+    {
+        return null;
+    }
+
+    /**
+    *summiert zwei Geldbetraege auf
+    *
+    *@param summand ein zu addierender Geldbetrag
+    *
+    *@return Geldbetrag ein aufsummierter Geldbetrag
+    */
+    public Geldbetrag summiere(Geldbetrag summand)
+    {
+        return summand;
+    }
+
+    public Geldbetrag subtrahiere(Geldbetrag subtrahend)
+    {
+
+        return subtrahend;
+    }
+
+    public Geldbetrag multiplizere(int faktor)
+    {
+        return new Geldbetrag(0, 0);
+    }
+
+    public int getEuroAnteil()
+    {
+        return _euroAnteil;
+    }
+
+    public int getCentAnteil()
+    {
+        return _centAnteil;
+    }
+
+    @Override
+    public String toString()
+    {
+        return getFormatiertenString();
+    }
+
+    public String getFormatiertenString()
+    {
+        return "";
+    }
+
+}
---
+++ Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
@@ -0,0 +1,140 @@
+package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class GeldbetragTest
+{
+    Geldbetrag _einEuroZwanzigCent = new Geldbetrag(1, 20);
+    Geldbetrag _nullEuroNullCent = new Geldbetrag(0, 0);
+    Geldbetrag _vierEuroEinCent = new Geldbetrag(4, 1);
+    Geldbetrag _maxEuroMaxCent = new Geldbetrag(Integer.MAX_VALUE, 99);
+    Map<String, Geldbetrag> werteMenge = new HashMap<String, Geldbetrag>();
+
+    public void testKonstruktor()
+    {
+        assertEquals(1, _einEuroZwanzigCent.getEuroAnteil());
+        assertEquals(20, _einEuroZwanzigCent.getCentAnteil());
+    }
+
+    @Test
+    public void testIstGueltigerString()
+    {
+
+        assertTrue(Geldbetrag.istGueltigerString("1,20 Euro"));
+        assertFalse(Geldbetrag.istGueltigerString("500a"));
+        assertTrue(Geldbetrag.istGueltigerString("01,20 Euro"));
+
+    }
+
+    @Test
+    public void testIstGueltigerEuroAnteil()
+    {
+        assertEquals(false, Geldbetrag.istGueltigerEuroAnteil(-1));
+        assertTrue(Geldbetrag.istGueltigerEuroAnteil(0));
+        assertTrue(Geldbetrag.istGueltigerEuroAnteil(1));
+        assertEquals(false,
+                Geldbetrag.istGueltigerEuroAnteil(Integer.MAX_VALUE + 1));
+    }
+
+    @Test
+    public void testIstGueltigerCentAnteil()
+    {
+        assertEquals(false, Geldbetrag.istGueltigerCentAnteil(-1));
+        assertTrue(Geldbetrag.istGueltigerCentAnteil(0));
+        assertTrue(Geldbetrag.istGueltigerCentAnteil(1));
+        assertEquals(false, Geldbetrag.istGueltigerCentAnteil(100));
+    }
+
+    @Test
+    public void testSelectGeldbetrag()
+    {
+        //int, int
+        assertEquals(false, werteMenge.containsKey("1,20 Euro"));
+        Geldbetrag.selectGeldbetrag(1, 20);
+        assertTrue(werteMenge.containsKey("1,20 Euro"));
+
+        //int
+        assertEquals(false, werteMenge.containsKey("2,20 Euro"));
+        Geldbetrag.selectGeldbetrag(220);
+        assertTrue(werteMenge.containsKey("1,20 Euro"));
+
+        //String
+        assertEquals(false, werteMenge.containsKey("3,20 Euro"));
+        Geldbetrag.selectGeldbetrag("3,20 Euro");
+        assertTrue(werteMenge.containsKey("1,20 Euro"));
+    }
+
+    @Test
+    public void testIstGroesserGleich()
+    {
+        assertTrue(_einEuroZwanzigCent.istGroesserGleich(_nullEuroNullCent));
+        assertTrue(_einEuroZwanzigCent.istGroesserGleich(_einEuroZwanzigCent));
+        assertTrue(_nullEuroNullCent.istGroesserGleich(_einEuroZwanzigCent));
+    }
+
+    @Test
+    public void testSummiere()
+    {
+        assertEquals(new Geldbetrag(5, 21),
+                _einEuroZwanzigCent.summiere(_vierEuroEinCent));
+        assertEquals(new Geldbetrag(0, 0),
+                _nullEuroNullCent.summiere(_nullEuroNullCent));
+        assertEquals(new Geldbetrag(1, 20),
+                _nullEuroNullCent.summiere(_einEuroZwanzigCent));
+    }
+
+    @Test
+    public void testSubtrahiere()
+    {
+        assertEquals(new Geldbetrag(5, 21),
+                _einEuroZwanzigCent.subtrahiere(_vierEuroEinCent));
+        assertEquals(new Geldbetrag(0, 0),
+                _nullEuroNullCent.subtrahiere(_nullEuroNullCent));
+        assertEquals(new Geldbetrag(1, 20),
+                _nullEuroNullCent.subtrahiere(_einEuroZwanzigCent));
+    }
+
+    @Test
+    public void testMultipliziere()
+    {
+        assertEquals(new Geldbetrag(2, 40),
+                _einEuroZwanzigCent.multiplizere(2));
+        assertEquals(new Geldbetrag(0, 0), _nullEuroNullCent.multiplizere(2));
+    }
+
+    @Test
+    public void testToString()
+    {
+        assertEquals("1,20 Euro", _einEuroZwanzigCent.toString());
+        assertEquals("0,00 Euro", _nullEuroNullCent.toString());
+    }
+
+    @Test
+    public void testGetEuroAnteil()
+    {
+        assertEquals(1, _einEuroZwanzigCent.getEuroAnteil());
+        assertEquals(4, _vierEuroEinCent.getEuroAnteil());
+    }
+
+    @Test
+    public void testGetCentAnteil()
+    {
+        assertEquals(20, _einEuroZwanzigCent.getEuroAnteil());
+        assertEquals(1, _vierEuroEinCent.getEuroAnteil());
+    }
+
+    @Test
+    public void testFormatiertenString()
+    {
+        assertEquals("1,20 Euro", _einEuroZwanzigCent.toString());
+        assertEquals("0,00 Euro", _nullEuroNullCent.toString());
+    }
+
+}
